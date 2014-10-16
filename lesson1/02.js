700c3414be49477d831257c7591661d5e1a117ec
crab <crab1127@gmail.com>
2014-10-16 17:24:02 +0800

    表单 与 指令

--- lesson1/02.js
+++ lesson1/02.js
@@ -1,7 +1,6 @@
 angular.module('drag', [])
   .directive('draggable', function($document) {
     var startX = 0, startY = 0, x = 0, y = 0;
-    console.log('docs');
     return function($scope, $element, $attr) {
       $element.css({
         position: 'relative',
@@ -31,7 +30,45 @@ angular.module('drag', [])
         $document.unbind('mousemove', mouseup);
       }
     }
+  })
+  .directive('myCurrentTime', function($timeout, dateFilter) {
+    console.log(1,$timeout);
+    console.log(2,dateFilter);
+    return function(scope, element, attrs) {
+      console.log(3,scope);
+      console.log(4,element);
+      console.log(5,attrs);
+      var format, //date format
+          timeoutId; //
+      //用于跟新ui
+      function updateTime() {
+        element.text(dateFilter(new Date(), format));
+      }
+
+      //监听，
+      scope.$watch(attrs.myCurrentTime, function(value) {
+        format = value;
+        updateTime();
+      });
+
+      function updateLater() {
+        timeoutId = $timeout(function() {
+          updateTime();
+          updateLater();
+        }, 1000)
+      }
+
+      element.bind('$destroy', function() {
+        $timeout.cancel(timeoutId);
+      });
+
+      updateLater();
+
+    }
   });
 function Ctrl1($scope) {
   $scope.name = 'angular';
+}
+function Ctrl2($scope) {
+  $scope.format = 'M/d/yy h:mm:ss a';
 }
